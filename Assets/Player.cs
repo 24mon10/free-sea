03deaea5664da3ac895b09a7ad498ec6a34f0301
hmj2422002 <hmj2422002@stu.o-hara.ac.jp>
2025-05-28 13:42:37 +0900

    モンスターの追加、プレイヤーのほぼ完成

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -20,6 +20,9 @@ public class Player : MonoBehaviour
 	[Header("落下の初速"), SerializeField]
 	private float _initFallSpeed = 2;
 
+	[Header("カメラ"), SerializeField]
+	private Camera m_targetCamera;
+
 	private Transform _transform;
 	private CharacterController _characterController;
 
@@ -28,10 +31,12 @@ public class Player : MonoBehaviour
 	private float _turnVelocity;
 	private bool _isGroundedPrev;
 
+	Animator animator; // アニメーション
 	public void OnMove(InputAction.CallbackContext context)
 	{
 		// 入力値を保持しておく
 		_inputMove = context.ReadValue<Vector2>();
+		animator.SetBool("Move", true);
 	}
 
 	public void OnJump(InputAction.CallbackContext context)
@@ -41,6 +46,7 @@ public class Player : MonoBehaviour
 		{
 			// ボタンが押された瞬間かつ着地している時だけ処理
 			if (!context.performed || !_characterController.isGrounded) return;
+			animator.SetTrigger("Jump");
 
 			// 鉛直上向きに速度を与える
 			_verticalVelocity = _jumpSpeed;
@@ -51,7 +57,13 @@ public class Player : MonoBehaviour
 	{
 		_transform = transform;
 		_characterController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+		if (m_targetCamera == null)
+		{
+			m_targetCamera = Camera.main;
+		}
 	}
+
 
 	private void Update()
 	{
@@ -74,12 +86,20 @@ public class Player : MonoBehaviour
 
 		_isGroundedPrev = isGrounded;
 
+		// カメラの向き(角度[deg])取得
+		var cameraAngleY = m_targetCamera.transform.eulerAngles.y;
+
+
 		// 操作入力と鉛直方向速度から、現在速度を計算
 		var moveVelocity = new Vector3(
 			_inputMove.x * _speed,
 			_verticalVelocity,
 			_inputMove.y * _speed
 		);
+
+		// カメラの角度分だけ移動量を回転
+		moveVelocity = Quaternion.Euler(0, cameraAngleY, 0) * moveVelocity;
+
 		// 現在フレームの移動量を移動速度から計算
 		var moveDelta = moveVelocity * Time.deltaTime;
 
@@ -88,10 +108,11 @@ public class Player : MonoBehaviour
 
 		if (_inputMove != Vector2.zero)
 		{
+			animator.SetBool("Move", true);
 			// 移動入力がある場合は、振り向き動作も行う
-
+			/*
 			// 操作入力からy軸周りの目標角度[deg]を計算
-			var targetAngleY = -Mathf.Atan2(_inputMove.y, _inputMove.x)
+			var targetAngleY = -Mathf.Atan2(moveVelocity.z, moveVelocity.x)
 				* Mathf.Rad2Deg + 90;
 
 			// イージングしながら次の回転角度[deg]を計算
@@ -104,6 +125,22 @@ public class Player : MonoBehaviour
 
 			// オブジェクトの回転を更新
 			_transform.rotation = Quaternion.Euler(0, angleY, 0);
+			*/
+
+			_transform.rotation = Quaternion.Lerp(
+				_transform.rotation,
+				Quaternion.LookRotation(Vector3.Scale(moveVelocity, new Vector3(1, 0, 1))),
+				0.1f);
 		}
+		else
+		{
+			animator.SetBool("Move", false);
+		}
+	}
+
+
+	private void Landing()
+	{
+		
 	}
 }
