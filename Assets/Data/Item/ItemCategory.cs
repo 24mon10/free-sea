b9825224a9190ff423f9231a12f9daec484b4774
hmj2422002 <hmj2422002@stu.o-hara.ac.jp>
2025-06-09 12:00:42 +0900

    アイテムのデータの追加(適宜追加予定)

---
+++ Assets/Data/Item/ItemCategory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCategory : MonoBehaviour 
+{
+	//アイテムの種別を定義
+
+	public enum Category
+	{
+		None,
+		ConsumableItem,
+		Wepon,
+		Armor,
+	}
+}
